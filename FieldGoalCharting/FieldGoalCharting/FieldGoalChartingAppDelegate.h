70f2bea6dc871bbf23cf1743a4902f1a7bb89687
Billy Janssen <dufootball11@gmail.com>
2011-11-16 11:37:55 -0600

    First Commit

--- FieldGoalCharting/FieldGoalCharting/FieldGoalChartingAppDelegate.h
+++ FieldGoalCharting/FieldGoalCharting/FieldGoalChartingAppDelegate.h
@@ -8,12 +8,15 @@
 
 #import <UIKit/UIKit.h>
 
-@class FieldGoalChartingViewController;
-
 @interface FieldGoalChartingAppDelegate : NSObject <UIApplicationDelegate>
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
 
-@property (nonatomic, retain) IBOutlet FieldGoalChartingViewController *viewController;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
 
 @end
