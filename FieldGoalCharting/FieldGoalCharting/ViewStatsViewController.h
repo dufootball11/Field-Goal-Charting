5c3714d82599bef3326cc10c064286979c13e326
Billy Janssen <dufootball11@gmail.com>
2011-11-27 23:18:30 -0600

    added the view stats vc

---
+++ FieldGoalCharting/FieldGoalCharting/ViewStatsViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewStatsViewController.h
+//  FieldGoalCharting
+//
+//  Created by Fries on 11/27/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class SegmentsController;
+
+@interface ViewStatsViewController : UIViewController {
+    SegmentsController *segmentsController;
+}
+@end
